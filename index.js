af57fbbfe360f5d88664bca8bb7ee5cd7b41e168
crashdaddy <kennethscoggins@mail.com>
2020-05-04 17:01:18 -0500

    fixed get

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const randoRouter = require('./routers/rando');
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use('/rando', randoRouter)
+app.use('/', randoRouter)
+
 
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
